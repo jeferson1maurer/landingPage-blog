389a1aaf413a9bb8a6fb4cca9383554cb5a726cd
jeferson <jeferson98luis@gmail.com>
2026-05-07 17:56:12 -0300

    chore: add seo em blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { getAllPosts } from "@/lib/posts";
 import { BlogList } from "@/templates/blog";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar o seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar o seu negócio",
+    url: "https://landing-page-blog-ten.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",    
+    images: [
+      {
+        url: "https://landing-page-blog-ten.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
     const sortedPosts = getAllPosts();
